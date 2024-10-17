002d1983a4e96e9a9807a9548d1553c9a4470fae
Kimberlynn01 <68210927+Kimberlynn01@users.noreply.github.com>
2024-10-17 04:13:52 +0000

    add user

--- user.js
+++ user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const createDBConnection = require("./database");
+const bcrypt = require("bcrypt");
 
 router.get("/", (req, res) => {
   const db = createDBConnection();
@@ -16,4 +17,25 @@ router.get("/", (req, res) => {
   db.end();
 });
 
+
+router.post("/add", (req, res) => {
+  const db = createDBConnection();
+  const { username, name, password } = req.body;
+
+  try{
+    const hasedPassword = await bcrypt.hash(password, 10);
+    
+    const sql = "INSERT INTO user (username, name, password) VALUES (?, ?, ?)";
+    db.query(sql, [username, name, hasedPassword], (err, results) => {
+      if(err){
+        return res.status(500).json({error: err.message});
+      }
+      return res.status(201).json({message: "User Successfulled Add!", userId: results.insertId});
+    });
+  }catch(error){
+    return res.status(500).json({error: "Error Add User"});
+  }finally{
+    db.end();
+  }
+});
 module.exports = router;
