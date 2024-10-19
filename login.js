d60b7e4fad92501254e0f50d10a25c7c254fc43d
danudiraja <danudiraja@gmail.com>
2024-10-20 00:56:50 +0700

    login function

--- login.js
+++ login.js
@@ -2,6 +2,7 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+const bcrypt = require("bcrypt"); // Tambahkan bcrypt untuk verifikasi password
 
 const app = express();
 
@@ -10,6 +11,16 @@ app.use(bodyParser.json());
 const SECRET_KEY = "8D46igOlkeUKGOjbeqFlUJa1H3xN16";
 const USER_API_URL = "https://apiflutterhbd-production.up.railway.app/api/v1/user?auth=chiquine";
 
+async function verifyPassword(inputPassword, hashedPassword) {
+  try {
+    const match = await bcrypt.compare(inputPassword, hashedPassword);
+    return match;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error verifying password");
+  }
+}
+
 app.post("/", async (req, res) => {
   const { username, password } = req.body;
 
@@ -17,15 +28,21 @@ app.post("/", async (req, res) => {
     const response = await axios.get(USER_API_URL);
     const users = response.data;
 
-    const user = users.find((u) => u.username === username && u.password === password);
+    const user = users.find((u) => u.username === username);
 
     if (user) {
-      const token = jwt.sign({ userId: user.id, username: user.username }, SECRET_KEY, { expiresIn: "1h" });
+      const passwordMatch = await verifyPassword(password, user.password);
 
-      res.json({
-        token: token,
-        name: user.name,
-      });
+      if (passwordMatch) {
+        const token = jwt.sign({ userId: user.id, username: user.username }, SECRET_KEY, { expiresIn: "1h" });
+
+        res.json({
+          token: token,
+          name: user.name,
+        });
+      } else {
+        res.status(401).json({ error: "Invalid username or password" });
+      }
     } else {
       res.status(401).json({ error: "Invalid username or password" });
     }
