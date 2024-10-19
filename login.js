ba240ba692ba5fb899a937050f8ed73b7271a979
danudiraja <danudiraja@gmail.com>
2024-10-20 01:17:06 +0700

    s;

--- login.js
+++ login.js
@@ -6,30 +6,25 @@ const bcrypt = require("bcryptjs");
 
 const app = express();
 
-// Middleware untuk mem-parsing JSON dan form-data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); // Ini untuk mendukung form-data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const SECRET_KEY = "8D46igOlkeUKGOjbeqFlUJa1H3xN16";
 const USER_API_URL = "https://apiflutterhbd-production.up.railway.app/api/v1/user?auth=chiquine";
 
-// Endpoint untuk login
 app.post("/", async (req, res) => {
-  const { username, password } = req.body; // Mengambil username dan password dari form-data
+  const { username, password } = req.body;
 
   try {
     const response = await axios.get(USER_API_URL);
     const users = response.data;
 
-    // Temukan user berdasarkan username
     const user = users.find((u) => u.username === username);
 
     if (user) {
-      // Periksa kecocokan password
       const isPasswordValid = bcrypt.compareSync(password, user.password);
 
       if (isPasswordValid) {
-        // Jika password valid, buat token JWT
         const token = jwt.sign({ userId: user.id, username: user.username }, SECRET_KEY, { expiresIn: "1h" });
 
         res.json({
