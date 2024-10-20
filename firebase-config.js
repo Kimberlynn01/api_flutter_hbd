98242c8ce065a4e2765a2fd608e717787adac6ae
danudiraja <danudiraja@gmail.com>
2024-10-20 16:48:40 +0700

    err

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,14 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./flutter-happybirtday-firebase-adminsdk-crfws-a25c4a2386.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: "gs://flutter-happybirtday.appspot.com", // Ganti dengan project Firebase Storage ID kamu
-});
+admin
+  .initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "gs://flutter-happybirtday.appspot.com",
+  })
+  .catch((error) => {
+    console.error("Firebase Admin SDK initialization error:", error);
+  });
 
 const bucket = admin.storage().bucket();
 
