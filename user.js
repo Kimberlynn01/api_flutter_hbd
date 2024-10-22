e502f4cd7f69ad15ff58b0ecf91d2623d20c754f
danudiraja <danudiraja@gmail.com>
2024-10-22 14:48:20 +0700

    ss

--- user.js
+++ user.js
@@ -5,6 +5,7 @@ const bcryptjs = require("bcryptjs");
 const multer = require("multer");
 const upload = multer();
 const jwt = require("jsonwebtoken");
+const FormData = require("form-data");
 
 const SECRET_KEY = "8D46igOlkeUKGOjbeqFlUJa1H3xN16";
 const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/dphacrvql/image/upload";
