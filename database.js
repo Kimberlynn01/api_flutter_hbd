4d5e7895dfb6e6cc5e4c8cc1a638abc6d7a96fd9
danudiraja <danudiraja@gmail.com>
2024-10-22 14:29:26 +0700

    sss

--- database.js
+++ database.js
@@ -1,8 +1,7 @@
-// const mysql = require("mysql2");
 const mysql = require("mysql2/promise");
 
-const createDBConnection = () => {
-  const db = mysql.createConnection({
+const createDBConnection = async () => {
+  const db = await mysql.createConnection({
     host: "autorack.proxy.rlwy.net",
     port: 17500,
     user: "root",
@@ -10,13 +9,7 @@ const createDBConnection = () => {
     database: "railway",
   });
 
-  db.connect((err) => {
-    if (err) {
-      console.error("Error connecting: " + err.stack);
-      return;
-    }
-    console.log("Terhubung ke database MySQL.");
-  });
+  console.log("Terhubung ke database MySQL.");
 
   return db;
 };
